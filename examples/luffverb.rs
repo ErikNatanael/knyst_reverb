641f64570901b2b0d53d2915a8f588c3132fe75c
Erik Natanael Gustafsson <erik@eriknatanael.com>
2023-11-11 11:51:06 +0100

    Reverb works!!

--- examples/luffverb.rs
+++ examples/luffverb.rs
@@ -8,6 +8,7 @@ use knyst::{
     modal_interface::commands,
     prelude::*,
     sphere::{KnystSphere, SphereSettings},
+    trig::interval_trig,
 };
 use knyst_reverb::luff_verb;
 use rand::{thread_rng, Rng};
@@ -24,31 +25,57 @@ fn main() -> Result<()> {
         print_error_handler,
     );
 
-    let verb = luff_verb(250 * 48);
-    graph_output(0, verb.repeat_outputs(1) * 0.1);
-
-    std::thread::sleep(std::time::Duration::from_millis(150));
-    for &freq in [400, 600, 500].iter().cycle() {
-        // new graph
-        commands().init_local_graph(commands().default_graph_settings());
-        let sig = sine().freq(freq as f32).out("sig") * 0.25;
+    let mut rng = thread_rng();
+    commands().init_local_graph(commands().default_graph_settings());
+    for freq in [300, 400, 500, 600, 700, 800].iter() {
+        let sig = sine().freq(*freq as f32).out("sig") * 0.25;
         let env = Envelope {
-            points: vec![(1.0, 0.005), (0.0, 0.5)],
-            stop_action: StopAction::FreeGraph,
+            points: vec![(1.0, 0.001), (0.0, 0.2)],
             ..Default::default()
         };
-        let sig = sig * handle(env.to_gen());
-
-        graph_output(0, sig.repeat_outputs(1));
-        // push graph to sphere
-        let graph = commands().upload_local_graph();
-
-        // graph_output(0, graph);
-        verb.input(graph.out(0) * 0.1);
-        std::thread::sleep(std::time::Duration::from_millis(2500));
+        let sig = sig
+            * handle(env.to_gen()).set(
+                "restart",
+                interval_trig().interval(rng.gen_range(1.5f32..3.5)),
+            );
+        graph_output(0, sig);
     }
+    let sig = commands().upload_local_graph();
+    let verb = luff_verb(2350 * 48, 0.65).lowpass(7000.).damping(4000.);
+    // .input(sig * 0.125);
+    // .input(sig * 0.125 + graph_input(0, 1));
+    verb.input(graph_input(0, 1));
+    // verb.input(sig * 0.125);
+    let sig = verb * 0.5;
+    // let sig = verb * 0.25 + (sig * 0.25);
+    graph_output(0, sig.repeat_outputs(1));
+
+    // std::thread::sleep(std::time::Duration::from_millis(150));
+    // for &freq in [400, 600, 500].iter().cycle() {
+    //     // new graph
+    //     commands().init_local_graph(commands().default_graph_settings());
+    //     let sig = sine().freq(freq as f32).out("sig") * 0.25;
+    //     let env = Envelope {
+    //         points: vec![(1.0, 0.005), (0.0, 0.5)],
+    //         stop_action: StopAction::FreeGraph,
+    //         ..Default::default()
+    //     };
+    //     let sig = sig * handle(env.to_gen());
+
+    //     graph_output(0, sig.repeat_outputs(1));
+    //     // push graph to sphere
+    // let graph = commands().upload_local_graph();
+
+    //     // graph_output(0, graph);
+    //     verb.input(graph.out(0) * 1.1);
+    //     std::thread::sleep(std::time::Duration::from_millis(2500));
+    // }
 
     // graph_output(0, (sine(wt).freq(200.)).repeat_outputs(1));
+
+    // let inspection = commands().request_inspection();
+    // let inspection = inspection.recv().unwrap();
+    // dbg!(inspection);
 
     let mut input = String::new();
     loop {
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 use std::time::SystemTime;
 
-use householder::random_householder;
 use knyst::{
+    filter::one_pole::*,
     prelude::{
         delay::{SampleDelay, StaticSampleDelay},
         impl_gen, Gen, GenState,
     },
     xorrng::XOrShift32Rng,
-    BlockSize, Sample,
+    BlockSize, Sample, SampleRate,
 };
+use matrix::random_householder;
 use rand::{seq::SliceRandom, thread_rng, Rng};
 struct Diffuser<const CHANNELS: usize> {
     delays: [StaticSampleDelay; CHANNELS],
@@ -43,8 +44,8 @@ fn hadamard<const N: usize>() -> [[Sample; N]; N] {
 impl<const CHANNELS: usize> Diffuser<CHANNELS> {
     pub fn new(max_delay_length_in_samples: usize) -> Self {
         let mut rng = thread_rng();
-        let mut flip_polarity = [1.0; CHANNELS];
-        flip_polarity[CHANNELS / 2..].fill(-1.);
+        let mut flip_polarity = [-1.0; CHANNELS];
+        flip_polarity[CHANNELS / 2..].fill(1.);
         flip_polarity.shuffle(&mut rng);
         let delays = std::array::from_fn(|i| {
             let time_min = (max_delay_length_in_samples / CHANNELS * i) as usize + 1;
@@ -67,24 +68,26 @@ impl<const CHANNELS: usize> Diffuser<CHANNELS> {
         input: &[Vec<Sample>; CHANNELS],
         output: &mut [Vec<Sample>; CHANNELS],
     ) {
-        let block_size = input.len();
+        let block_size = input[0].len();
         for f in 0..block_size {
             // Get the output of the delay
             let mut sig = [0.0; CHANNELS];
             for channel in 0..CHANNELS {
                 sig[channel] = self.delays[channel].read() * self.flip_polarity[channel];
-                self.delays[channel].write(input[channel][f]);
+                self.delays[channel].write_and_advance(input[channel][f]);
             }
-            let mut sig2 = [0.0; CHANNELS];
-            // Apply Hadamard matrix
-            for row in 0..CHANNELS {
-                for column in 0..CHANNELS {
-                    // TODO: Vectorise
-                    sig2[row] += sig[column] * self.hadamard_matrix[row][column];
-                }
-            }
+            matrix::hadamard_recursive(&mut sig);
+            // let mut sig2 = [0.0; CHANNELS];
+            // // Apply Hadamard matrix
+            // for row in 0..CHANNELS {
+            //     for column in 0..CHANNELS {
+            //         // TODO: Vectorise
+            //         sig2[row] += sig[column] * self.hadamard_matrix[row][column];
+            //     }
+            // }
             for channel in 0..CHANNELS {
-                output[channel][f] = sig2[channel];
+                output[channel][f] = sig[channel];
+                // output[channel][f] = input[channel][f];
             }
         }
     }
@@ -95,62 +98,84 @@ struct Tail<const CHANNELS: usize> {
     feedback_gain: Sample,
     /// Size is the length of the delay
     delays: [StaticSampleDelay; CHANNELS],
+    lowpasses: [OnePoleLpf; CHANNELS],
     /// One block of samples
     process_temp_buffers: [Vec<Sample>; CHANNELS],
-    householder_matrix: [[Sample; CHANNELS]; CHANNELS],
+    process_temp_buffers1: [Vec<Sample>; CHANNELS],
 }
 
 impl<const CHANNELS: usize> Tail<CHANNELS> {
     pub fn new(delay_length_in_samples: usize, feedback: Sample) -> Self {
-        let time_min = delay_length_in_samples / 2;
+        let time_min = delay_length_in_samples / 10;
         let time_max = delay_length_in_samples;
         let mut rng = thread_rng();
         let delays = std::array::from_fn(|i| {
             let delay_time = rng.gen_range(time_min..time_max);
             StaticSampleDelay::new(delay_time)
         });
+        let lowpasses = std::array::from_fn(|_| OnePoleLpf::new());
         Self {
             feedback_gain: feedback,
             process_temp_buffers: std::array::from_fn(|_| vec![0.0; 0]),
+            process_temp_buffers1: std::array::from_fn(|_| vec![0.0; 0]),
             delays,
-            householder_matrix: random_householder(),
+            lowpasses,
         }
     }
     /// Init internal buffers to the block size. Not real time safe.
     pub fn init(&mut self, block_size: usize) {
         self.process_temp_buffers = std::array::from_fn(|_| vec![0.0; block_size]);
+        self.process_temp_buffers1 = std::array::from_fn(|_| vec![0.0; block_size]);
     }
     pub fn process_block(
         &mut self,
         input: &[Vec<Sample>; CHANNELS],
         output: &mut [Vec<Sample>; CHANNELS],
+        damping: &[Sample],
+        sample_rate: SampleRate,
     ) {
         // Get the output of the delay
         for (i, delay) in self.delays.iter_mut().enumerate() {
             delay.read_block(&mut self.process_temp_buffers[i]);
         }
         // Set output to the output of the delay
-        for channel in 0..CHANNELS {
-            output[channel].copy_from_slice(&self.process_temp_buffers[channel]);
-        }
-        // apply feedback to output of delay
-        for i in 0..CHANNELS {
-            for sample in &mut self.process_temp_buffers[i] {
-                *sample *= self.feedback_gain;
-            }
+        for (output_channel, process_channel) in output.iter_mut().zip(&self.process_temp_buffers) {
+            output_channel.copy_from_slice(process_channel);
         }
         // TODO: Combine gain and matrix
-        // mix matrix, householder
-        // todo!("Mix householder");
+        // Apply Hadamard matrix
+        let block_size = input[0].len();
+        for f in 0..block_size {
+            let mut chan = [0.0; CHANNELS];
+            for (c, channel) in self.process_temp_buffers.iter().enumerate() {
+                chan[c] = channel[f];
+            }
+            matrix::Householder::in_place(&mut chan);
+            for (c, channel) in self.process_temp_buffers.iter_mut().enumerate() {
+                channel[f] = chan[c];
+            }
+        }
+        // apply feedback to output of delay
+        for (i, channel) in self.process_temp_buffers.iter_mut().enumerate() {
+            for sample in channel.iter_mut() {
+                *sample *= self.feedback_gain;
+            }
+            self.lowpasses[i].process(
+                sample_rate,
+                channel,
+                damping,
+                &mut self.process_temp_buffers1[i],
+            );
+        }
         // add together with input
-        for (process_channel, input_channel) in self.process_temp_buffers.iter_mut().zip(input) {
+        for (process_channel, input_channel) in self.process_temp_buffers1.iter_mut().zip(input) {
             for (process_s, input_s) in process_channel.iter_mut().zip(input_channel) {
                 *process_s += *input_s;
             }
         }
         // Pipe back into the delay
         for (channel, delay) in self.delays.iter_mut().enumerate() {
-            delay.write_block(&self.process_temp_buffers[channel]);
+            delay.write_block_and_advance(&self.process_temp_buffers1[channel]);
         }
     }
 }
@@ -160,19 +185,21 @@ const DIFFUSERS: usize = 4;
 pub struct LuffVerb {
     diffusers: [Diffuser<CHANNELS>; DIFFUSERS],
     tail: Tail<CHANNELS>,
+    input_lpf: OnePoleLpf,
     buffer0: [Vec<Sample>; CHANNELS],
     buffer1: [Vec<Sample>; CHANNELS],
 }
 #[impl_gen]
 // impl<const DIFFUSERS: usize, const CHANNELS: usize> LuffVerb<{DIFFUSERS}, {CHANNELS}> {
 impl LuffVerb {
-    pub fn new(tail_delay: usize) -> Self {
-        let diffusers = std::array::from_fn(|i| Diffuser::new(tail_delay / DIFFUSERS));
+    pub fn new(tail_delay: usize, feedback: Sample) -> Self {
+        let diffusers = std::array::from_fn(|i| Diffuser::new(tail_delay / (DIFFUSERS * 2)));
         Self {
             diffusers,
-            tail: Tail::new(tail_delay, 0.2),
+            tail: Tail::new(tail_delay, feedback),
             buffer0: std::array::from_fn(|_| Vec::new()),
             buffer1: std::array::from_fn(|_| Vec::new()),
+            input_lpf: OnePoleLpf::new(),
         }
     }
     pub fn init(&mut self, block_size: BlockSize) {
@@ -183,37 +210,61 @@ impl LuffVerb {
             d.init(*block_size);
         }
     }
-    pub fn process(&mut self, input: &[Sample], output: &mut [Sample]) -> GenState {
+    pub fn process(
+        &mut self,
+        input: &[Sample],
+        output: &mut [Sample],
+        lowpass: &[Sample],
+        damping: &[Sample],
+        sample_rate: SampleRate,
+    ) -> GenState {
         // Use buffer0 and buffer1 as input and output buffers every other time to cut down on the number of buffers needed.
         let mut in_buf = &mut self.buffer0;
         let mut out_buf = &mut self.buffer1;
-        // Fill all channels of buffer0 with the input
+
+        self.input_lpf.process(sample_rate, input, lowpass, output);
+        // Fill all channels of buffer0 with the in,
         for channel in in_buf.iter_mut() {
-            channel.copy_from_slice(input);
+            channel.copy_from_slice(output);
         }
-        for diffuser in &mut self.diffusers {
+        for (i, diffuser) in &mut self.diffusers.iter_mut().enumerate() {
             diffuser.process_block(in_buf, out_buf);
             std::mem::swap(&mut in_buf, &mut out_buf);
         }
-        let early_reflections_amount = 0.3;
-        for (out_sample, out_channel) in output.iter_mut().zip(out_buf.iter()) {
-            *out_sample = out_channel.iter().sum::<f32>() * early_reflections_amount;
+        std::mem::swap(&mut in_buf, &mut out_buf);
+        output.fill(0.0);
+        let early_reflections_amount = 0.5;
+        for (f, out_sample) in output.iter_mut().enumerate() {
+            for channel in out_buf.iter_mut() {
+                *out_sample += channel[f];
+            }
+            *out_sample *= early_reflections_amount;
         }
         std::mem::swap(&mut in_buf, &mut out_buf);
-        self.tail.process_block(in_buf, out_buf);
+        self.tail
+            .process_block(in_buf, out_buf, damping, sample_rate);
         // Sum output channels
-        let compensation_amp = 1.0 / CHANNELS as f32;
+        let compensation_amp = 1.0 / (CHANNELS as f32 * DIFFUSERS as f32);
         for (f, out_sample) in output.iter_mut().enumerate() {
             for channel in out_buf.iter_mut() {
                 *out_sample += channel[f];
             }
             *out_sample *= compensation_amp;
         }
+        // assert_eq_slices(output, &out_buf[0]);
         GenState::Continue
     }
 }
 
-mod householder {
+fn assert_eq_slices(s0: &[Sample], s1: &[Sample]) {
+    for (v0, v1) in s0.iter().zip(s1) {
+        assert_eq!(*v0, *v1);
+    }
+}
+
+mod matrix {
+    use std::marker::PhantomData;
+
     use knyst::Sample;
     use rand::{thread_rng, Rng};
     use rand_distr::StandardNormal;
@@ -240,6 +291,39 @@ mod householder {
         }
         // Use uv as input to create Householder
         householder(uv)
+    }
+
+    pub fn hadamard_recursive(frame: &mut [Sample]) {
+        if frame.len() <= 1 {
+            return;
+        }
+        let d = frame.len() / 2;
+        hadamard_recursive(&mut frame[..d]);
+        hadamard_recursive(&mut frame[d..]);
+        for i in 0..d {
+            let a = frame[i];
+            let b = frame[i + d];
+            frame[i] = a + b;
+            frame[i + d] = a - b;
+        }
+    }
+
+    pub struct Householder<const CHANNELS: usize> {
+        _channels: PhantomData<[(); CHANNELS]>,
+    }
+    impl<const CHANNELS: usize> Householder<CHANNELS> {
+        const MULTIPLIER: f64 = -2. / CHANNELS as f64;
+        #[inline]
+        pub fn in_place(frame: &mut [Sample; CHANNELS]) {
+            let mut sum: f64 = 0.0;
+            for f in frame.iter_mut() {
+                sum += *f as f64;
+            }
+            sum *= Householder::<CHANNELS>::MULTIPLIER;
+            for f in frame.iter_mut() {
+                *f += sum as f32;
+            }
+        }
     }
 }
 
